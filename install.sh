37761fa4613beb3c001e12147fccb19e8d201579
Frederic Roggon <53627251+CodeAdminDe@users.noreply.github.com>
2026-03-05 17:20:44 +0100

    Refactor install.sh for better environment handling
    Updated the installation script for oh-my-zsh to handle local and devcontainer environments more effectively. Improved echo messages and ensured non-interactive installation for different environments.

--- install.sh
+++ install.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
-OH_MY_ZSH_SETUP=https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+OH_MY_ZSH_SETUP="https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
+LOCAL="no"
 
-#sudo if needed
-sudo -v
+# local / devcontainer
+# Devcontainer default env does have REMOTE_CONTAINERS=true
+if [ "$REMOTE_CONTAINERS" != "true" ]; then
+    LOCAL="yes"
+    echo "Local env detected. Asking for sudo permissions ..."
+    sudo -v
+else
+    echo "Devcontainer env detected. Skipping sudo permission request ..."
+fi
 
 # (Re-)Install oh-my-zsh
-echo "(Re-)Install oh-my-zsh"
-rm -rf $HOME/.oh-my-zsh
-curl -L $OH_MY_ZSH_SETUP | sh
+echo "(Re-)Install oh-my-zsh..."
+rm -rf "$HOME/.oh-my-zsh"
+
+# Non-interactive installation for envs other than local
+# CHSH=no: Disables trying to set default shlett via chsh
+# RUNZSH=no: Disables trying to open a zsh-session by default.
+curl -fsSL $OH_MY_ZSH_SETUP | CHSH=$LOCAL RUNZSH=$LOCAL sh
 
 # symlink .zshrc
-echo "symlink .zshrc"
-rm $HOME/.zshrc
-ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
-echo "Installation done!"
+echo "Set symlink for .zshrc..."
+rm -f "$HOME/.zshrc" # (-f)orce, even if not exists to avoid interrput.
+ln -s "$HOME/.dotfiles/.zshrc" "$HOME/.zshrc"
+
+echo "Installation successfully! 🎉"
